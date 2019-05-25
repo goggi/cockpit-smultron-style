b3327fdb482cf82bb532fde3bec1b16aefe797eb
Goran <goran@crawlyfi.com>
2019-05-25 23:58:20 +0200

    fix

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,19 @@
 <?php
 if (COCKPIT_ADMIN && !COCKPIT_API_REQUEST) {
 
+    $settings = $app->config['crawlyfi'] ?? [];
+    extract($settings);
+
+
+    $app['app.assets.base'] = array_merge($app['app.assets.base'], [
+        '/addons/cockpit-smultron-style/assets/css/crawlyfi.css'
+    ]);
+
+    $app['app.assets.base'] = array_merge($app['app.assets.base'], [
+        "/addons/cockpit-smultron-style/assets/css/system-{$system}.css"
+    ]);    
+    
+
     $this->on('admin.init', function () use ($app) {
 
         $settings = $app->config['crawlyfi'] ?? [];
