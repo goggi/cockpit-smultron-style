7829c314e5599f206ff492538b677ee0b82d0ac7
Goran <goran@crawlyfi.com>
2019-06-11 13:27:20 +0200

    added force full title

--- bootstrap.php
+++ bootstrap.php
@@ -11,8 +11,7 @@ if (COCKPIT_ADMIN && !COCKPIT_API_REQUEST) {
 
     $app['app.assets.base'] = array_merge($app['app.assets.base'], [
         "/addons/cockpit-smultron-style/assets/css/system-{$system}.css"
-    ]);    
-    
+    ]);
 
     $this->on('admin.init', function () use ($app) {
 
@@ -24,7 +23,7 @@ if (COCKPIT_ADMIN && !COCKPIT_API_REQUEST) {
         // Load environment specific css.
         if (!empty($system) && $app->path("cockpit-smultron-style:assets/css/system-{$system}.css")) {
             $this->helper('admin')->addAssets("cockpit-smultron-style:assets/css/system-{$system}.css");
-        }        
+        }
 
       });
 
