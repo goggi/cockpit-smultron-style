413711c920bc369d2ea77044dc62703137b0c937
Goran <goran@crawlyfi.com>
2019-08-09 10:17:44 +0200

    fixed size of text

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,6 @@ if (COCKPIT_ADMIN && !COCKPIT_API_REQUEST) {
     $settings = $app->config['crawlyfi'] ?? [];
     extract($settings);
 
-
     $app['app.assets.base'] = array_merge($app['app.assets.base'], [
         '/addons/cockpit-smultron-style/assets/css/crawlyfi.css'
     ]);
@@ -19,6 +18,8 @@ if (COCKPIT_ADMIN && !COCKPIT_API_REQUEST) {
         extract($settings);
 
         $this->helper('admin')->addAssets('cockpit-smultron-style:assets/css/crawlyfi.css');
+        $this->helper('admin')->addAssets('cockpit-smultron-style:assets/js/app.utils.js');
+
 
         // Load environment specific css.
         if (!empty($system) && $app->path("cockpit-smultron-style:assets/css/system-{$system}.css")) {
